64c48be127fedde9fc60b7c109755e32e41173bb
Kanhaiyasahu01 <kanhaiyasahutools@gmail.com>
2025-02-04 08:21:00 +0530

    fix cors

--- server/index.js
+++ server/index.js
@@ -16,7 +16,8 @@ const allowedOrigins = [
 	"http://localhost:5173",
 	// "https://assetmate-tle-smashers.netlify.app"
 	// "https://asset-mate-tle-smashers.vercel.app/",
-	"https://assetmate-kanhaiya.netlify.app"
+	"https://assetmate-kanhaiya.netlify.app",
+	"https://manage-your-assets-tle-smashers.vercel.app/"
 ];
 
 // CORS configuration
